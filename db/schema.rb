18043d38f4014e8d1a974e28074c79c85600a450
Craig Kahle <kahle@sxsw.com>
2019-01-18 18:07:10 -0600

    simplify everything, store customer id to charge later

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_30_181348) do
+ActiveRecord::Schema.define(version: 2019_01_19_000420) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -70,6 +70,7 @@ ActiveRecord::Schema.define(version: 2018_12_30_181348) do
     t.string "reset_token"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "stripe_customer_id"
   end
 
   add_foreign_key "cart_items", "carts"
